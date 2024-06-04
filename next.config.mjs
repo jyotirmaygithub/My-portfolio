2798d895ff70f1d8aad5c8cea211be6360ac75e9
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-06-04 21:33:04 +0530

    Image and alteration in next config : to enable working of the image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // to make the url work in the nextjs in the image component of it we need to write these below thigns 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
